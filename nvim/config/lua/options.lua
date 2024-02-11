43250a2a9b5016ec64b9e51881fe0ca65ad72e10
Fritiof Rusck <fritiof@rusck.se>
2024-02-11 12:19:26 +0100

    feat: change neo tree design

--- nvim/config/lua/options.lua
+++ nvim/config/lua/options.lua
@@ -1,3 +1,8 @@
+vim.fn.sign_define("DiagnosticSignError", { text = " ", texthl = "DiagnosticSignError" })
+vim.fn.sign_define("DiagnosticSignWarn", { text = " ", texthl = "DiagnosticSignWarn" })
+vim.fn.sign_define("DiagnosticSignInfo", { text = " ", texthl = "DiagnosticSignInfo" })
+vim.fn.sign_define("DiagnosticSignHint", { text = "󰌵", texthl = "DiagnosticSignHint" })
+
 -- :help options
 local options = {
   backup = false,                         -- creates a backup file
--- nvim/config/lua/plugins/neo-tree.lua
+++ nvim/config/lua/plugins/neo-tree.lua
@@ -1,22 +1,38 @@
 require("neo-tree").setup({
-    close_if_last_window = true,
-    window = {
+  close_if_last_window = true,
+  window = {
     width = 30,
     mappings = {
-        ["h"] = "close_node",
-        ["l"] = "open_with_window_picker",
-        ["w"] = nil,
+      ["h"] = "close_node",
+      ["l"] = "open_with_window_picker",
+      ["w"] = nil,
     },
-    },
-    filesystem = {
+  },
+  filesystem = {
     follow_current_file = true,
     hijack_netrw_behavior = "open_current",
+    default_component_configs = {},
+    git_status = {
+      symbols = {
+        -- Change type
+        added = "", -- or "✚", but this is redundant info if you use git_status_colors on the name
+        modified = "", -- or "", but this is redundant info if you use git_status_colors on the name
+        deleted = "✖", -- this can only be used in the git_status source
+        renamed = "󰁕", -- this can only be used in the git_status source
+        -- Status type
+        untracked = "",
+        ignored = "",
+        unstaged = "󰄱",
+        staged = "",
+        conflict = "",
+      },
+    },
     use_libuv_file_watcher = true,
     filtered_items = {
-        never_show = { -- remains hidden even if visible is toggled to true, this overrides always_show
+      never_show = { -- remains hidden even if visible is toggled to true, this overrides always_show
         ".DS_Store",
         "thumbs.db",
-        },
+      },
     },
-    },
+  },
 })
