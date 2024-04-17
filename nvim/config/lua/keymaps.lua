8df51475bd576728ebbc81b50b70c465fec79707
Fritiof Rusck <fritiof@rusck.se>
2024-04-17 10:04:05 +0200

    feat: revert new keymaps

--- nvim/config/lua/keymaps.lua
+++ nvim/config/lua/keymaps.lua
@@ -64,12 +64,12 @@ wk.register({
 	["h"] = { "<cmd>noh<CR>", "Hide selected" },
 	["/"] = { "<cmd>CommentToggle<CR>", "Comment toggle" },
 
-	["f"] = { "<cmd>Telescope find_files<cr>", "Find File Name" },
-	["w"] = { "<cmd>Telescope live_grep<cr>", "Live Grep" },
 
 	-- === Telescope ===
 	["F"] = {
 		name = "+Find",
+		f = { "<cmd>Telescope find_files<cr>", "File Name" },
+		w = { ":Telescope live_grep<CR>", "Words" },
 		s = { ":Telescope lsp_workspace_symbols<CR>", "Workspace Symbols" },
 		d = { ":Telescope diagnostics<CR>", "Buffer Diagnostics" },
 		b = { ":Telescope buffers<CR>", "Buffers" },
