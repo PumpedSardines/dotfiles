8b51f30c18a109f58853a68b8324e6e19a644706
Fritiof Rusck <fritiof@rusck.se>
2024-06-27 11:56:31 +0200

    feat: data

--- flake.nix
+++ flake.nix
@@ -3,8 +3,8 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-    nix-darwin.url = "github:LnL7/nix-darwin";
-    nix-darwin.inputs.nixpkgs.follows = "nixpkgs";
+    darwin.url = "github:LnL7/nix-darwin";
+    darwin.inputs.nixpkgs.follows = "nixpkgs";
     home-manager = {
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
@@ -14,23 +14,26 @@
   outputs = {
     nixpkgs,
     home-manager,
-    nix-darwin,
+    darwin,
     ...
   }: let
     system = "aarch64-darwin";
     pkgs = nixpkgs.legacyPackages.${system};
   in {
     darwinConfigurations = {
-      "Fritiofs-MacBook-Pro" = nix-darwin.lib.darwinSystem {
+      "Fritiofs-MacBook-Pro" = darwin.lib.darwinSystem {
         system = system;
+        pkgs = import nixpkgs { system = "aarch64-darwin"; };
         modules = [
           ./configuration.nix
           home-manager.darwinModules.home-manager
           {
+            # users.users.fritiofrusck.name = "fritiofrusck";
             # users.users.fritiofrusck.home = "/Users/fritiofrusck";
             home-manager.useGlobalPkgs = true;
             home-manager.useUserPackages = true;
-            home-manager.users.fritiofrusck = import ./home.nix;
+            home-manager.users.fritiofrusck.imports = [ ./home.nix ];
+            # home-manager.users.fritiofrusck = import ./home.nix;
 
             # Optionally, use home-manager.extraSpecialArgs to pass
             # arguments to home.nix
