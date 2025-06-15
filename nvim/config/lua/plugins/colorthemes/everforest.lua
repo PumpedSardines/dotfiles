219e18c902c78ddbd11f82841d046fc5338ae71b
Fritiof Rusck <fritiof@rusck.se>
2025-06-15 13:21:59 +0200

    Update configuration

--- nvim/config/lua/plugins/colorthemes/everforest.lua
+++ nvim/config/lua/plugins/colorthemes/everforest.lua
@@ -11,11 +11,11 @@ vim.cmd([[
 --   undercurl = true,
 --   sp = '#FFA500', -- change to your desired color
 -- })
-require("everforest").setup({
-  on_highlights = function(hl, palette)
-    hl.DiagnosticError = { fg = palette.none, bg = palette.none, sp = palette.red }
-    hl.DiagnosticWarn = { fg = palette.none, bg = palette.none,  sp = palette.yellow }
-    hl.DiagnosticInfo = { fg = palette.none, bg = palette.none,  sp = palette.blue }
-    hl.DiagnosticHint = { fg = palette.none, bg = palette.none,  sp = palette.green }
-  end,
-})
+-- require("everforest").setup({
+--  on_highlights = function(hl, palette)
+--    hl.DiagnosticError = { fg = palette.none, bg = palette.none, sp = palette.red }
+--    hl.DiagnosticWarn = { fg = palette.none, bg = palette.none,  sp = palette.yellow }
+--    hl.DiagnosticInfo = { fg = palette.none, bg = palette.none,  sp = palette.blue }
+--    hl.DiagnosticHint = { fg = palette.none, bg = palette.none,  sp = palette.green }
+--  end,
+-- })
