55971d88e337f7968a4a8f43514e1082f2a0eaed
fritiof <fritiof@rusck.se>
2025-06-21 18:41:57 +0200

    Generation #89

--- modules/rofi/rofi.nix
+++ modules/rofi/rofi.nix
@@ -3,6 +3,6 @@
     enable = true;
     package = pkgs.rofi-wayland;
     configPath = "/home/fritiof/.config/rofi/config.rasi";
-    theme = "/etc/nixos/modules/rofi/config/style.rasi";
+    theme = "/home/fritiof/nixos/modules/rofi/config/style.rasi";
   };
 }
