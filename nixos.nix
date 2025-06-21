f7985046e291808bb924bd529694bff8a4f5b86c
fritiof <fritiof@rusck.se>
2025-06-21 22:45:18 +0200

    Generation #104

--- nixos.nix
+++ nixos.nix
@@ -16,6 +16,7 @@
 
     # NixOS specific
     ./modules/hypr/hypr.nix
+    ./modules/nwg/nwg.nix
     ./modules/network-manager/network-manager.nix
     ./modules/dolphin/dolphin.nix
     ./modules/electron-flags/electron-flags.nix
