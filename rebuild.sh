948c3d3602dd3dd580df415dd32ddc36ea4b0e59
fritiof <fritiof@rusck.se>
2025-06-21 14:28:39 +0200

    Generation 80

--- rebuild.sh
+++ rebuild.sh
@@ -1,1 +1,5 @@
+GENERATION=$(sudo nix-env -p /nix/var/nix/profiles/system --list-generations | tail -n1 | awk '{print $1}')
+NEW_GENERATION=$(($GENERATION + 1))
+git add .
+git commit -m "Generation $NEW_GENERATION"
 sudo nixos-rebuild switch --flake .#fritiof-old-dell
