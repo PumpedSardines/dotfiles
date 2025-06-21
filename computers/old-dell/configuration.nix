3bfaaacc59be9cb2d04cc7243b1470b49fd03f7e
fritiof <fritiof@rusck.se>
2025-06-22 00:12:18 +0200

    Generation #110

--- computers/old-dell/configuration.nix
+++ computers/old-dell/configuration.nix
@@ -39,9 +39,7 @@
     packages = with pkgs; [];
   };
 
-  services.xserver.enable = true;
-  # services.xserver.layout = "custom";
-
+  # services.xserver.enable = true;
   services.xserver.xkb.extraLayouts.custom = {
     description = "US layout with ao on AltGr+[";
     languages = ["eng"];
