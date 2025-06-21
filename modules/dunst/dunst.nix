f0f3d8fe3c4eb3357e81093c17eaae7dd1f5649f
fritiof <fritiof@rusck.se>
2025-06-21 21:34:36 +0200

    Generation #97

--- modules/dunst/dunst.nix
+++ modules/dunst/dunst.nix
@@ -1,9 +1,10 @@
-{ pkgs, ... }: {
+{ pkgs, config, ... }: {
   home.packages = with pkgs; [
     libnotify
   ];
   services.dunst = {
     enable = true;
-    configFile = ./dunstrc;
+    configFile = config.lib.file.mkOutOfStoreSymlink "/home/fritiof/nixos/modules/dunst/dunstrc";
+    # configFile = ./dunstrc;
   };
 }
