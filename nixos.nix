6da8274f5b58452f19572321decb102e0d1c89cd
fritiof <fritiof@rusck.se>
2025-06-21 18:33:09 +0200

    Generation #87

--- nixos.nix
+++ nixos.nix
@@ -12,7 +12,10 @@
     ./modules/lazygit/lazygit.nix
     ./modules/wezterm/wezterm.nix
     ./modules/misc/misc.nix
+
+    # NixOS specific
     ./modules/hypr/hypr.nix
+    ./modules/nautilus/nautilus.nix
     ./modules/electron-flags/electron-flags.nix
     ./modules/dunst/dunst.nix
     ./modules/waybar/waybar.nix
