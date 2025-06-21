e7c833046b324d4dcb02f8f9d222d22ceef13d17
fritiof <fritiof@rusck.se>
2025-06-21 14:45:34 +0200

    Generation #81

--- modules/hypr/hypr.nix
+++ modules/hypr/hypr.nix
@@ -8,7 +8,7 @@
   ];
   home.file = {
     ".config/hypr" = {
-      source = config.lib.file.mkOutOfStoreSymlink /etc/nixos/modules/hypr/config;
+      source = config.lib.file.mkOutOfStoreSymlink ./modules/hypr/config;
       # source = ./config;
       recursive = true;
     };
