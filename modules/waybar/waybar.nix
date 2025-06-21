7d475853716970048b4abd3e0fea2ed4d993880a
fritiof <fritiof@rusck.se>
2025-06-21 16:41:56 +0200

    Generation #84

--- modules/waybar/waybar.nix
+++ modules/waybar/waybar.nix
@@ -2,7 +2,8 @@
   programs.waybar.enable = true;
   home.file = {
     ".config/waybar" = {
-      source = config.lib.file.mkOutOfStoreSymlink /etc/nixos/modules/waybar/config;
+      source = config.lib.file.mkOutOfStoreSymlink /home/fritiof/nixos/modules/waybar/config;
+      # source = ./config;
       recursive = true;
     };
   };
