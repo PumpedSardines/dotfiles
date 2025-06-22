e079a7cd0589bee8ccbdda34ff8fe736e1774d25
fritiof <fritiof@rusck.se>
2025-06-22 11:05:32 +0200

    Generation #116

--- modules/quickshell/quickshell.nix
+++ modules/quickshell/quickshell.nix
@@ -1,5 +1,12 @@
-{ quickshell, ... }: {
+{ config, quickshell, ... }: {
   home.packages = [
     quickshell
   ];
+  home.file = {
+    ".config/quickshell" = {
+      source = config.lib.file.mkOutOfStoreSymlink /home/fritiof/nixos/modules/quickshell/config;
+      # source = ./config;
+      recursive = true;
+    };
+  };
 }
