a6edcb10805ee8a4223df1ee7ce56e19194d8845
fritiof <fritiof@rusck.se>
2025-06-23 22:30:08 +0200

    Generation #121

--- computers/old-dell/nvidia.nix
+++ computers/old-dell/nvidia.nix
@@ -39,8 +39,9 @@
     # Optionally, you may need to select the appropriate driver version for your specific GPU.
     package = config.boot.kernelPackages.nvidiaPackages.stable;
 
-    environment.systemPackages = with pkgs; [
-      lshw
-    ];
   };
+
+  environment.systemPackages = with pkgs; [
+    lshw
+  ];
 }
