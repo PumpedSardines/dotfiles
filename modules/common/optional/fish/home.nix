e793171bdb6e859872f07b8ddf44da0b72d2026c
Fritiof Rusck <fritiof@rusck.se>
2025-06-24 14:19:38 +0200

    refactor

--- modules/common/optional/fish/home.nix
+++ modules/common/optional/fish/home.nix
@@ -13,6 +13,25 @@
         description = "Custom fish prompt";
         body = "printf '%s%s%s > ' (set_color $fish_color_cwd) (prompt_pwd) (set_color normal)";
       };
+      wezterm-theme = {
+        description = "Set the wezterm theme";
+        argumentNames = ["theme"];
+        body = ''
+          set -l theme_path ~/.config/wezterm/theme.lua
+
+          if [ $theme = "dark" ]
+            echo "return \"Everforest Dark\"" > $theme_path
+            return
+          end
+
+          if [ $theme = "light" ]
+            echo "return \"Everforest Light\"" > $theme_path
+            return
+          end
+
+          echo "Invalid argument \"$theme\", can be either dark or light"
+        '';
+      };
     };
   };
 }
