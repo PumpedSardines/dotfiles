9c0433357ca72f251dba2ec3e7100500b0613768
Fritiof Rusck <fritiof@rusck.se>
2025-06-24 16:36:47 +0200

    refactor

--- modules/common/optional/wezterm/config/reload-theme.lua
+++ modules/common/optional/wezterm/config/reload-theme.lua
@@ -4,17 +4,21 @@ local is_darwin = function()
   return wezterm.target_triple:find("darwin") ~= nil
 end
 
-local get_dark_mode = function()
+local get_dark_mode = function ()
+  local default_dark_mode = true
+
   if is_darwin() then
-    local script = "osascript -e 'tell application \"System Events\" to tell appearance preferences to return dark mode'"
-    local handle = io.popen(script)
-    if not handle then
-      return nil, "Failed to execute command"
+    local dark_theme_file = os.getenv("HOME") .. "/.config/dark-theme"
+    local f = io.open(dark_theme_file, "r")
+    if f == nil then
+      return default_dark_mode
     end
-    local result = handle:read("*a")
-    handle:close()
-    return result == "true\n"
+    local content = f:read("*a")
+    f:close()
+    return content == "1\n"
   end
+
+  return default_dark_mode
 end
 
 local get_theme_name = function()
@@ -29,13 +33,11 @@ local iter = function()
   window:set_config_overrides({
     color_scheme = get_theme_name(),
   })
-  wezterm.time.call_after(5.0, function()
+  wezterm.time.call_after(1.0, function()
     iter()
   end)
 end
 
-wezterm.on('window-config-reloaded', function(window, _)
-  iter()
-end)
+iter()
 
 return get_theme_name()
