19902f8fdd029d5760afc9aed0079742b198e23d
Fritiof Rusck <fritiof@rusck.se>
2025-06-25 07:30:54 +0200

    refactor

--- modules/common/optional/tmux-status-line/home.nix
+++ modules/common/optional/tmux-status-line/home.nix
@@ -1,21 +1,12 @@
 { pkgs, ... }: let
   tmux-status-line = let
     repo = pkgs.lib.cleanSource ./.;
-    frameworks = pkgs.darwin.apple_sdk.frameworks;
     manifest = (builtins.fromTOML (builtins.readFile "${repo}/Cargo.toml")).package;
   in
     pkgs.rustPlatform.buildRustPackage {
       pname = manifest.name;
       version = manifest.version;
-      buildInputs = [
-        frameworks.Security
-        frameworks.CoreFoundation
-        frameworks.CoreServices
-        frameworks.ApplicationServices
-        frameworks.SystemConfiguration
-        frameworks.CoreVideo
-        frameworks.AppKit
-      ];
+      buildInputs = [];
       cargoLock.lockFile = "${repo}/Cargo.lock";
       src = repo;
     };
